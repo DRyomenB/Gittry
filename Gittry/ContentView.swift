0d6e393c8aee19744d73c02357de548ec276e632
Bleck <bleckdastine8@gmail.com>
2025-03-08 15:43:56 -0500

    This is the new change

--- Gittry/ContentView.swift
+++ Gittry/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "waterfall")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, je suis en train d'essayer github pour la premiere fois!")
         }
         .padding()
     }
